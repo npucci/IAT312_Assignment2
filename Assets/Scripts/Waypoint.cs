39c80f827c93579f2e08a1cc0c2609bb4f8af54c
SYuennan <steven.yuennan@gmail.com>
2017-03-05 15:26:49 -0800

    Add files via upload

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Waypoint : MonoBehaviour {
+
+    private int loadNextLevel;
+
+	// Use this for initialization
+	void Start () {
+        loadNextLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void OnTriggerStay2D(Collision2D Other) {
+    if (Input.GetKeyDown(KeyCode.W))
+     {
+        Application.LoadLevel(loadNextLevel + 1);
+     } 
+    }
+}

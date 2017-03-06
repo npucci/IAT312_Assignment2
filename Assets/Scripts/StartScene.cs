c2b32cf9ddd5a6aa99d9674166d2e8708dd39d9d
Niccolo Pucci <npucci@sfu.ca>
2017-03-05 20:51:59 -0800

    Added Level3, Start Menu, and Bug Fixes/Prefabs

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour {
+	public enum LEVEL {EASY, NORMAL, HARD};
+	public static LEVEL Level;
+	public int Sequence;
+	private float Timer;
+	private bool jumpflag=false;
+
+
+	public void StartGame(){
+		Application.LoadLevel (1);//change the variable according to your build settings^^
+	}
+
+	/*
+	public void EasyMode(){
+		Level = LEVEL.EASY;
+		StartGame (1);
+	}
+	public void NormalMode(){
+		Level = LEVEL.NORMAL;
+		StartGame (2);
+	}
+	public void HardMode(){
+		Level = LEVEL.HARD;
+		StartGame (3);
+	}
+
+	public void QuitButton()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+	}
+
+
+
+
+	void Start () {
+
+	}
+	*/	
+	void Update () {
+		Timer += Time.deltaTime;
+		//Debug.Log (Timer);
+		int tmp = (int)(Timer*2.5);
+		if (tmp % 3 == Sequence && !jumpflag) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y+0.1f, transform.position.z);
+
+			jumpflag = !jumpflag;
+		}
+		if (tmp % 3 != Sequence && jumpflag) {
+			jumpflag = !jumpflag;
+		}
+
+	}
+}
+
+
+

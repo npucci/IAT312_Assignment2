fddbc843df32e3f04a55c65a60aeee05130d490a
Yitian Zhang <skyler2zhang@gmail.com>
2017-03-17 09:23:56 -0700

    falling_obstacle_fix

--- Assets/Scripts/falling_obstacle.cs
+++ Assets/Scripts/falling_obstacle.cs
@@ -6,28 +6,44 @@ public class falling_obstacle : MonoBehaviour {
 	public PlayerController player;
 	public float height;
 	public float speed,distance;
-	public bool fall;
+	public bool fall,lift;
+	public PlayerController Player;
+	public float attackDamage;
 
 	// Use this for initialization
 	void Start () {
-		
+		lift = false;
 	}
-	
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.name == "Player") {
+			col.gameObject.GetComponent <Health>().decreaseHp(attackDamage);
+		}
+
+
+	}
 	// Update is called once per frame
 	void Update () {
 		if (Mathf.Abs(player.getposition ().x-gameObject.transform.localPosition.x) < distance) {
 			fall = true;
 		}
 
-		else {
-			fall = false;
-		}
 
 	}
 	void FixedUpdate(){
 		if (fall) {
-			transform.Translate(Vector3.down * speed *Time.deltaTime);
+			transform.Translate (Vector3.down * speed * Time.deltaTime);
+			fall = false;
+			//lift = true;
 		}
+		if (lift) {
+			transform.Translate (Vector3.up * speed * Time.deltaTime);
+			lift = false;
+		}
+
+
+
 
 	}
 }

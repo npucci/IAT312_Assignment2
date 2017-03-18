1429ef3bdeb8d1b310b0d595749867073ded408f
Yitian Zhang <skyler2zhang@gmail.com>
2017-03-18 13:49:40 -0700

    add the torch and the material and ...

---
+++ Assets/Scripts/Flashing_light.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashing_light : MonoBehaviour {
+	private bool isshine;
+	public float wait_time;
+	private IEnumerator interval(){
+		yield return new WaitForSeconds(wait_time);
+		isshine = true ;
+	}
+	// Use this for initialization
+	void Start () {
+		isshine = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isshine) {
+			if (transform.GetComponent<Light> ().range < 10) {
+				transform.GetComponent<Light> ().range = transform.GetComponent<Light> ().range + 0.1f;
+			} else {
+				isshine = false;
+				StartCoroutine (interval ());
+			}
+		} else {
+			if (transform.GetComponent<Light> ().range > 0) {
+				transform.GetComponent<Light> ().range = transform.GetComponent<Light> ().range - 0.1f;
+			}
+
+		}
+	}
+}

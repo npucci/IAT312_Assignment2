e776eef31ecef6b246519b757e9a511fcd07672c
SYuennan <steven.yuennan@gmail.com>
2017-03-18 23:08:14 -0700

    Add files via upload

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour {
+
+    public GameObject ProjectilePrefabs;
+    public Transform ProjectileInstantiate;
+
+    public float fireRate;
+
+    public Timer fireTimer;
+
+    private bool fire = false;
+
+	// Use this for initialization
+	void Start () {
+
+        fireTimer = new Timer(fireRate);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        fireTimer.updateTimer(Time.deltaTime);
+
+        if (!fire && fireTimer.stopped()) {
+            fire = true;
+        }
+
+        FireProjectile();
+	}
+
+    public void FireProjectile() {
+        if (fire)
+        {
+
+            GameObject enemyProjectile = Instantiate(ProjectilePrefabs, ProjectileInstantiate.transform.position, ProjectileInstantiate.rotation);
+            fireTimer.startTimer();
+            fire = false;
+
+        }
+    }
+
+    public bool isFiring() {
+        return fire;
+    }
+}
---
+++ Assets/Scripts/EnemyProjectileMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileMovement : MonoBehaviour {
+
+    private Vector2 moveX;
+    private Rigidbody2D rb;
+
+    public float fireDistance = 30.0f;
+    public float proDamage = 1.0f;
+    public float torque;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        moveX = new Vector2(fireDistance, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(moveX, ForceMode2D.Impulse);
+        rb.AddTorque(torque, ForceMode2D.Impulse);
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Contains("Player"))
+        {
+
+            other.gameObject.GetComponent<Health>().decreaseHp(proDamage);
+            Destroy(gameObject);
+        }
+    }
+}

1f0c65847ce990a82c4b98bd80357954dee7ebfb
Yitian Zhang <skyler2zhang@gmail.com>
2017-03-19 15:40:12 -0700

    change the final boss script and add the new book shelter

--- Assets/Scripts/Alchemist.cs
+++ Assets/Scripts/Alchemist.cs
@@ -6,44 +6,29 @@ public class Alchemist : MonoBehaviour {
 	
 	public float Attack_Interval = 0.5f;
 	private Timer AttackTimer;
-	private SpriteRenderer sr;
 	public bool isattacking;
 	private Animator anim;
-	private bool fire = false;
 
 	public GameObject FireballPrefabs;
 	public Transform FireballInstantiatePoint;
 	// Use this for initialization
 	void Start () {
-		sr = GetComponent<SpriteRenderer> ();
-		AttackTimer = new Timer (Attack_Interval);
 		anim = GetComponent<Animator> ();
+		isattacking = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isattacking) {
 			anim.CrossFade ("attack", 0f);
-			fire = true;
 			AttackTimer.startTimer ();
 		} 
 		else {
-			fire = false;
+			anim.CrossFade ("idle", 0f);
 		}
-		InstantiateFireballs();
-		
 	}
 
-	public void InstantiateFireballs() {
-		if (fire == true)
-		{
-			GameObject Arrow;
-			Arrow = Instantiate(FireballPrefabs, FireballInstantiatePoint.transform.position, FireballInstantiatePoint.rotation) as GameObject;
-
-		} 
-		else if (AttackTimer.stopped())
-		{
-			fire = false;
-		}
+	public bool iffire(){
+		return isattacking;
 	}
 }
---
+++ Assets/Scripts/HP_Alchemist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class HP_Alchemist : MonoBehaviour {
+
+	private Health Health;
+	public Sprite[] HPState;
+	public float scalex,index; 
+	public Slider targetSliderOject; 
+
+	float getratio(){
+		return Health.getHealth () / Health.getMaxHealth ();
+	}
+	// Use this for initialization
+	void Start () {
+		Health = GameObject.Find ("Alchemist_Enemy").GetComponent<Health> ();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		targetSliderOject.value= getratio (); 
+
+
+	}
+	void FixedUpdate() {
+	}
+}
+

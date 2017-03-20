649c4c7ce0631bd3443545646d7204b54c442be5
SYuennan <steven.yuennan@gmail.com>
2017-03-19 21:47:01 -0700

    Add files via upload

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    private SpriteRenderer sr;
+    private float playerPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        playerPos = GameObject.Find("Player").transform.position.x;
+
+        if (transform.position.x > playerPos)
+        {
+            sr.flipX = true;
+        }
+        else {
+            sr.flipX = false;
+        }
+
+    }
+}
